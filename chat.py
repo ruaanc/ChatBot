e27e1d9c3290a1abf8ffdeb3659a48c6b63688a7
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-09-30 22:02:39 -0300

    Up

--- chat.py
+++ chat.py
@@ -4,8 +4,6 @@ from chatterbot import ChatBot
 bot = ChatBot("Meu Bot")
 
 conv = (
-   "Quais são os custos para abertura de conta ? ",
-   "Pioneira em taxa zero no mercado de títulos públicos e renda fixa privada, a Easynvest disponibiliza também outros investimentos com taxas diferenciadas. Confira abaixo nossos custos. Renda Variável: Na Easynvest não cobramos taxa de custódia, somente é cobrada a taxa de corretagem, que é de R$ 4,99 por ordem executada, e R$ 2,49 para lote fracionário."
 )
 
 trainer = ListTrainer(bot)
